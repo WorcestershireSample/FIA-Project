2418346c28acc6e20b4e646cfa2b8e06923a50b0
salle142 <119331697+salle142@users.noreply.github.com>
2022-12-04 00:33:46 -0500

    Add files via upload

---
+++ pytestcomma.py
@@ -0,0 +1,83 @@
+# tessseract stuff
+from PIL import Image
+import pytesseract
+
+# SQL stuff
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+word = (pytesseract.image_to_string(Image.open('brownine_mix2.png')))
+# word = 'Testing 1, testing 2, testing 3, testing 4(testing 6, testing 7)'
+print(word)
+word = word.replace('INGREDIENTS: ','')
+word = word.replace('\n',' ')
+word = word.replace(', ',',')
+word = word.replace('.','')
+word = word.replace(' OR ',',')
+
+# paranthesis treated as commas
+word = word.replace('),', ',')
+word = word.replace('(', ',')
+word = word.replace('[', ',')
+word = word.replace('],', ',')
+
+
+# paranthesis if going to be dealt with
+# word = word.replace('),',')\n')
+# word = word.replace(' (','(')
+
+
+# for some reason, there is an extra linespace at the end of the word
+# print(word)
+# print("hello!\n")
+# balance = 0
+# counter=-1
+# deal with subingredients
+# for i in word:
+#     counter +=1
+#     if (i=='('):
+#         balance += 1
+#     if (i == ')'):
+#         balance -= 1
+#     if balance == 0 and i == ',' and word[counter-1] != ')':
+#         word = word[:counter] + '\n' + word[counter+1:]
+
+# remove subingredients
+
+
+
+words = word.split(",")
+words = [*set(words)]
+ingredients = [item.strip() for item in words]
+
+cnx = mysql.connector.connect(user='salle142', password='College05#',
+                              host='localhost', database = 'fia')
+
+mycursor = cnx.cursor()
+
+# run sql commands
+mycursor.execute("")
+
+def has_value(cursor, value):
+    query = 'SELECT * from foodlistvegan WHERE itemName = %s'
+    cursor.execute(query, (value,))
+    row = cursor.fetchone()
+    if (row):
+        if (row[1] == 1):
+            print(value + ' may be vegan.')
+        else:
+            print(value + ' is not vegan.')
+    else:
+        print(value + ' is not in the database!')
+    return row
+
+for i in ingredients:
+    if (i.find('(')):
+        x =i.partition('(')
+        # print(x[0])
+    # print(x[0])
+    has_value(mycursor, x[0])
+cnx.close()
