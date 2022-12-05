8789297dd7fbe5724e2596009e07559c668d1af9
Christopher Lipovsky <clipo124@live.kutztown.edu>
2022-12-05 07:41:06 -0500

    added more filtering to the filter_image_string function

--- image_to_string_filtering.py
+++ image_to_string_filtering.py
@@ -6,11 +6,16 @@ from PIL import Image
 #  add tesseract to path variable on both operating systems to ignore this line
 #  pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
+#  filter_image_string takes the string output of pytesseract.image_to_string()
+#  and returns an array of strings. Each string in the array is 1 ingredient
 def filter_image_string(pytesseract_string):
+    #  buffer_to_string is a helper function for filter_image_string
+    #  that takes an array of characters and converts it to a string
+    #  to be appended to the array of strings returned in the outer function
     def buffer_to_string(a):
         new_string = ""
         for elt in a:
-            if elt == '\n':  # pytesseract adds \n chars in the string for reason unknown to me
+            if elt == '\n':  # pytesseract adds \n chars in the string for reason unknown to me - Chris
                 new_string += " "
             else:
                 new_string += elt
@@ -31,6 +36,11 @@ def filter_image_string(pytesseract_string):
             buffer.clear()
         else:
             buffer.append(character)
+
+    for element in array:
+        index = element.find("INGREDIENTS: ")
+        if index != -1:
+            array[index] = array[index].replace("INGREDIENTS: ", "")
 
     return array
 
