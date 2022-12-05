9ff35224e35da193eb674dde570a46a59a2ea35c
Christopher Lipovsky <clipo124@live.kutztown.edu>
2022-12-05 00:55:36 -0500

    made the script running on Mac and Windows

--- image_to_string_filtering.py
+++ image_to_string_filtering.py
@@ -1,31 +1,36 @@
+import os
+
 import pytesseract
 from PIL import Image
 
-# need this if tesseract.exe is not found in PATH variable
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+#  need this if tesseract is not found in PATH variable
+#  os.path.join utilizes file system of the operating system running the program
+#  and joins the tesseract folder
+
+pytesseract.pytesseract.tesseract_cmd = os.path.join("tesseract")
 
 
 def filter_image_string(pytesseract_string):
     def buffer_to_string(a):
-        newString = ""
+        new_string = ""
         for elt in a:
             if elt == '\n':  # pytesseract adds \n chars in the string for reason unknown to me
-                newString += " "
+                new_string += " "
             else:
-                newString += elt
-        return newString
+                new_string += elt
+        return new_string
 
     buffer = []
     array = []
-    subIngredient = 0
+    sub_ingredient = 0
     for character in pytesseract_string:
         if character == '(':
-            subIngredient = 1
+            sub_ingredient = 1
         if character == ')':
-            subIngredient = 0
-        if character == ',' and subIngredient == 1:
+            sub_ingredient = 0
+        if character == ',' and sub_ingredient == 1:
             continue
-        if character == ',' and subIngredient == 0:
+        if character == ',' and sub_ingredient == 0:
             array.append(buffer_to_string(buffer))
             buffer.clear()
         else:
