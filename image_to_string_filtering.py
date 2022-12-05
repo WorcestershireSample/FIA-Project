0ff66a51f7344a7643d452e0c5fd989bd26bd4a2
Christopher Lipovsky <clipo124@live.kutztown.edu>
2022-12-05 00:55:36 -0500

    Failed to make script work on mac and windows using os module that interfaces with the operating python is running on

--- image_to_string_filtering.py
+++ image_to_string_filtering.py
@@ -1,31 +1,29 @@
 import pytesseract
 from PIL import Image
 
-# need this if tesseract.exe is not found in PATH variable
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-
 
 def filter_image_string(pytesseract_string):
     def buffer_to_string(a):
-        newString = ""
+        new_string = ""
         for elt in a:
             if elt == '\n':  # pytesseract adds \n chars in the string for reason unknown to me
-                newString += " "
+                new_string += " "
             else:
-                newString += elt
-        return newString
+                new_string += elt
+        return new_string
 
     buffer = []
     array = []
-    subIngredient = 0
+    sub_ingredient = 0
     for character in pytesseract_string:
         if character == '(':
-            subIngredient = 1
+            sub_ingredient = 1
         if character == ')':
-            subIngredient = 0
-        if character == ',' and subIngredient == 1:
+            sub_ingredient = 0
+        if character == ',' and sub_ingredient == 1:
             continue
-        if character == ',' and subIngredient == 0:
+        if character == ',' and sub_ingredient == 0:
             array.append(buffer_to_string(buffer))
             buffer.clear()
         else:
